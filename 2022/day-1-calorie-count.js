218ce01f21a59730269cc7541b52e76dad0abdee
Alex Mostaghni <113554195+mostaghz@users.noreply.github.com>
2022-12-09 23:35:23 -0700

    code along with example numbers to start

--- 2022/day-1-calorie-count.js
+++ 2022/day-1-calorie-count.js
@@ -1,1 +1,18 @@
-//
+// In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+
+// import calorie list
+// divide calorie list into array of groups
+// find sum of each list groups
+// find sum of the largest list group
+
+// let numberGroups = sampleInput.split("\n\n");
+let sampleGroup = "1000\n2000\n3000";
+
+let getSumOfGroup = (group)  => {
+    let strNumList = group.split("\n");
+    let numList = strNumList.map((str) => Number(str))
+    let numSum = numList.reduce((sum, num) => sum + num, 0)
+    return numSum;
+};
+
+console.log(getSumOfGroup(sampleGroup));
