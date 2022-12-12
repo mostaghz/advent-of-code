129235572851406ad8f136abc9f67c4746e0e1af
Alex Mostaghni <113554195+mostaghz@users.noreply.github.com>
2022-12-11 23:42:26 -0700

    some messy af word vomit of pseudo code for day 2

---
+++ 2022/day-2/rock-paper-scissors.js
@@ -0,0 +1,10 @@
+// 1 for Rock, 2 for Paper, and 3 for Scissors plus 0 if you lost, 3 if the round was a draw, and 6 if you won
+
+// GUESSING WITH MESSY PSEUDO CODE BEFORE CODE ALONG
+
+// look at the fetch cards example with George -- taking objects in array and sorting them by house... take the elf answer pairs and sort them by rock, paper, and scissors?
+
+// or assign values to answer pairs with GIANT for loop? let answer pairs = whatever point value. if a === thrown && y === thrown, console.log(numberOfPoints), else if b === thrown && x === thrown console.log(numberOfPoints)... would need to have a bunch of these to account for draws and losses... woof
+
+// or assign values to answer pairs with forEach? for each win, do this; for each loss, do this; for each draw, do this... this might be better? but then how would I total the points for selection...
+
