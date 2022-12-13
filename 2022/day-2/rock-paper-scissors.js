0e152ea9279d4e948fa6ba93ba247100da400123
Alex Mostaghni <113554195+mostaghz@users.noreply.github.com>
2022-12-12 21:13:14 -0700

    day 2 part 2 solution

--- 2022/day-2/rock-paper-scissors.js
+++ 2022/day-2/rock-paper-scissors.js
@@ -1,3 +1,5 @@
+// PART 1
+
 let sampleInput = `A Y
 B X
 C Z`;
@@ -12,10 +14,12 @@ Y is paper (2 points)
 Z is scissors (3 points)
 */
 
+let sumReducer = (sum, num) => sum + num;
+
 // write a function to get the game score and move score of a game
 let sampleGame = 'A Y';
 
-// use when testing in the browser... sampleInput on 55 is now puzzleInput
+// // use when testing in the browser... sampleInput on 59 is now puzzleInput
 const summonPuzzleInput = async (day) => {
     let response = await fetch("https://adventofcode.com/2022/day/2/input");
     let responseText = await response.text();
@@ -56,8 +60,48 @@ let games = puzzleInput.split("\n");
 let scores = games.map(getTheScore);
 
 // add them all up
-let totalScore = scores.reduce((sum, num) => sum + num, 0);
+let totalScore = scores.reduce(sumReducer, 0);
 
-console.log(totalScore)
 
-// browser console shows 11475
+// PART 2
+
+/*
+X is lose
+Y is draw
+Z is win
+*/
+
+// more look up tables
+let requiredMoves = {
+    A: {X: "S", Y: "R", Z: "P"},
+    B: {X: "R", Y: "P", Z: "S"},
+    C: {X: "P", Y: "S", Z: "R"}
+};
+
+let actualMoveValues = {R: 1, P: 2, S: 3};
+
+let actualGameValues = {X: 0, Y: 3, Z: 6};
+
+let getTheActualScore = (game) => {
+    let [opponentMove, gameOutcome] = game.split(" ");
+
+    // find which move needs to be played
+    let yourMove = requiredMoves[opponentMove][gameOutcome];
+
+    // get the game score
+    let gameScore = actualGameValues[gameOutcome];
+
+    // get the move score
+    let moveScore = actualMoveValues[yourMove];
+
+    // add them up
+    return gameScore + moveScore
+};
+
+let actualScores = games.map(getTheActualScore);
+let actualTotalScore = actualScores.reduce(sumReducer, 0)
+
+console.log(actualTotalScore)
+
+// browser console shows 11475 for part 1
+// browser console shows 16862 for part 2
