42ab224ff263b19478efd1a4ca84632c4453a1b0
Alex Mostaghni <113554195+mostaghz@users.noreply.github.com>
2022-12-12 20:50:06 -0700

    day 2 part 1 solution

--- 2022/day-2/rock-paper-scissors.js
+++ 2022/day-2/rock-paper-scissors.js
@@ -1,10 +1,63 @@
-// 1 for Rock, 2 for Paper, and 3 for Scissors plus 0 if you lost, 3 if the round was a draw, and 6 if you won
+let sampleInput = `A Y
+B X
+C Z`;
 
-// GUESSING WITH MESSY PSEUDO CODE BEFORE CODE ALONG
+/*
+A is rock
+B is paper
+C is scissors
 
-// look at the fetch cards example with George -- taking objects in array and sorting them by house... take the elf answer pairs and sort them by rock, paper, and scissors?
+X is rock (1 point)
+Y is paper (2 points)
+Z is scissors (3 points)
+*/
 
-// or assign values to answer pairs with GIANT for loop? let answer pairs = whatever point value. if a === thrown && y === thrown, console.log(numberOfPoints), else if b === thrown && x === thrown console.log(numberOfPoints)... would need to have a bunch of these to account for draws and losses... woof
+// write a function to get the game score and move score of a game
+let sampleGame = 'A Y';
 
-// or assign values to answer pairs with forEach? for each win, do this; for each loss, do this; for each draw, do this... this might be better? but then how would I total the points for selection...
+// use when testing in the browser... sampleInput on 55 is now puzzleInput
+const summonPuzzleInput = async (day) => {
+    let response = await fetch("https://adventofcode.com/2022/day/2/input");
+    let responseText = await response.text();
+    return responseText.trim();
+};
 
+let puzzleInput = await summonPuzzleInput();
+
+
+// look up tables - simpler than lots of if else statements
+let gameValues = {
+    A: {X: 3, Y: 6, Z: 0},
+    B: {X: 0, Y: 3, Z: 6},
+    C: {X: 6, Y: 0, Z: 3}
+};
+
+let moveValues = {X: 1, Y: 2, Z: 3};
+
+let getTheScore = (game) => {
+    // destructuring and more direct assignment
+    let [opponentMove, yourMove] = game.split(" ");
+
+    // get the game score (win, lose, or draw)
+    let gameScore = gameValues[opponentMove][yourMove];
+
+    // get the move score
+    let moveScore = moveValues[yourMove];
+
+    // add them up
+    return gameScore + moveScore;
+
+    // property value shorthand - use with objects when key and value are the same (used before look up table happened)
+    // return { opponentMove, yourMove}
+};
+
+// get the scores of all the games
+let games = puzzleInput.split("\n");
+let scores = games.map(getTheScore);
+
+// add them all up
+let totalScore = scores.reduce((sum, num) => sum + num, 0);
+
+console.log(totalScore)
+
+// browser console shows 11475
