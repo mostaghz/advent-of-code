df70977f0f5d65381d0d50fff733997a76a759fe
Alex Mostaghni <113554195+mostaghz@users.noreply.github.com>
2022-12-14 15:56:10 -0700

    day 3 part 2 solution

--- 2022/day-3/rucksacks.js
+++ 2022/day-3/rucksacks.js
@@ -19,6 +19,8 @@ let sumReducer = (sum, num) => sum + num;
 
 let rucksacks = puzzleInput.split("\n");
 
+// PART 1
+
 // find the common item for each rucksack
 let sampleRucksack = 'vJrwpWtwJgWrhcsFMMfFFhFp';
 
@@ -50,6 +52,39 @@ let commonItems = rucksacks.map(findCommonItem);
 let itemPriorities = commonItems.map(getItemPriority);
 let prioritySum = itemPriorities.reduce(sumReducer, 0);
 
-console.log(prioritySum)
+// PART 2
+
+// divide rucksacks into groups of three (recursive function = function that calls itself... similar to a loop)
+let getGroupsOf3 = (arr) =>
+    arr.length ? [arr.slice(0, 3), ...getGroupsOf3(arr.slice(3))] : [];
+
+// find common item for each group of three
+let sampleGroup = [
+        'vJrwpWtwJgWrhcsFMMfFFhFp',
+        'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+        'PmmdzqPrVvPwwTWBwg'
+];
+
+let findCommonItemInGroupOf3 = ([sack1, sack2, sack3]) => {
+    // convert first two groups into sets
+    let [set1, set2] = [new Set(sack1), new Set(sack2)];
+
+    // find item in group 3 that's in both sets
+    let commonItem = [...sack3].find((item) => set1.has(item) && set2.has(item));
+
+    return commonItem
+};
+
+
+// find priorities
+
+// add them all up
+let prioritySumOfGroups = getGroupsOf3(rucksacks)
+    .map(findCommonItemInGroupOf3)
+    .map(getItemPriority)
+    .reduce(sumReducer, 0)
+
+console.log(prioritySumOfGroups)
 
 // part 1 outputs 7674
+// part 2 outputs 2805
